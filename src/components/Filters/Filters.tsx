0b802c805489738529a63206914787081b727d49
Timoteo Ladmann <teoladmann@gmail.com>
2020-01-31 21:19:21 +0100

    feat: add filters

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,14 +1,62 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Filters.css';
 
-const Filters: React.FC = () => (
-  <div className="filters-container">
-    <div className="filter-elements-container">
-      <div className="filter-button">Dates</div>
-      <div className="filter-button">Tags</div>
+interface FiltersProps {
+  filter: boolean;
+  option: string;
+  toggleFilter: any;
+  toggleOption: any;
+}
+interface Date {
+  id: string;
+  text: string;
+}
+
+const Filters: React.FC<FiltersProps> = ({
+  filter,option,toggleFilter,toggleOption,
+}) => {
+  const dateFilters: any = [
+    { id : 'today',text : 'Today' },
+    { id : 'week',text : 'This week' },
+    { id : 'month',text : 'Last 30 days' },
+    { id : 'three-months',text : 'Last 3 months' },
+    { id : 'year',text : 'Last year' },
+    { id : 'forever',text : 'Forever and always' },
+  ];
+  return (
+    <div className="filters-container">
+      <div className="filter-elements-container">
+        <button type="button" id="dates" className="button-filter" onClick={toggleFilter}>
+          <div className={`filter-container filter-button ${filter ? 'selected' : ''}`}>
+            Dates
+          </div>
+        </button>
+      </div>
+      <div className={`dates-filter-container ${filter ? 'show' : 'not-show'}`}>
+        {dateFilters.map((e:Date) => (
+          <button type="button" id={`date-${e.id}`} className="button-filter " onClick={toggleOption}>
+            <div className={`filter-container filter-option ${option === `date-${e.id}` ? 'option-selected' : ''}`}>
+              {e.text}
+            </div>
+          </button>
+        ))}
+        <button type="button" id="date-custom" className="button-filter">
+          <div className="filter-container filter-option">
+            Custom
+          </div>
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+Filters.propTypes = {
+  filter : PropTypes.bool.isRequired,
+  option : PropTypes.string.isRequired,
+  toggleFilter : PropTypes.func.isRequired,
+  toggleOption : PropTypes.func.isRequired,
+};
 
 export default Filters;
--- src/pages/ResultsLanding/ResultsLanding.tsx
+++ src/pages/ResultsLanding/ResultsLanding.tsx
@@ -1,39 +1,38 @@
 import React,{ useState,useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 import HeaderLogged from '../../components/HeaderLogged/HeaderLogged';
 import Filters from '../../components/Filters/Filters';
 import './ResultsLanding.css';
 import ResultsContainer from '../../containers/ResultsContainer/ResultsContainer';
 import { fetchUserHistory } from '../../ApiClient';
-import { createBrowserHistory } from 'history';
 
 const ResultsLanding: React.FC = () => {
-  console.log(useLocation());
   const [results,setResults] = useState([]);
   const [query,setQuery] = useState(useLocation().search.slice(5));
+  const [filter,setFilter] = useState(false);
+  const [option,setOption] = useState('');
+
   const history = createBrowserHistory();
-  // const searchString = ;
   const pathName = useLocation().pathname;
   let searchString = '';
   if (pathName !== '/search') {
     searchString = pathName.replace('/search/','');
-    console.log(searchString);
   }
-  useEffect(() => {
-    setQuery(searchString);
-    fetchUserHistory(searchString)
-      .then((res:any) => res.json())
-      .then((res:any) => {
-        setResults(res.results);
-      });
-    // setUpEvents(res);
+  // useEffect(() => {
+  //   setQuery(searchString);
+  //   fetchUserHistory(searchString)
+  //     .then((res:any) => res.json())
+  //     .then((res:any) => {
+  //       setResults(res.results);
+  //     });
+  //   // setUpEvents(res);
 
-    // .catch((e:any) => console.log(e));
-  }, []); //eslint-disable-line
-
+  //   // .catch((e:any) => console.log(e));
+  // }, []); //eslint-disable-line
   const updateQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const searchString = e.target.value;
+    searchString = e.target.value;
     setQuery(searchString);
     fetchUserHistory(searchString)
       .then((res:any) => res.json())
@@ -46,10 +45,27 @@ const ResultsLanding: React.FC = () => {
       });
   };
 
+  const toggleFilter = () => {
+    if (filter) {
+      setFilter(false);
+      setOption('');
+    } else {
+      setFilter(true);
+    }
+  };
+
+  const toggleOption = (e: React.MouseEvent<HTMLElement>) => {
+    if (option !== e.currentTarget.id) {
+      setOption(e.currentTarget.id);
+    } else {
+      setOption('');
+    }
+  };
+
   return (
     <>
       <HeaderLogged updateQuery={updateQuery} query={query} />
-      <Filters />
+      <Filters option={option} toggleOption={toggleOption} filter={filter} toggleFilter={toggleFilter} />
       <ResultsContainer results={results} />
     </>
   );
